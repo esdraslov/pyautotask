2bf048f48ebe9b49942f26aca5bc51ea16a19d4f
esdraslov <103470150+esdraslov@users.noreply.github.com>
2023-07-10 23:02:28 +0000

    created click function

--- src/epack/pyautotask.py
+++ src/epack/pyautotask.py
@@ -1,1 +1,9 @@
-import win32api
+import win32api, win32con
+import time
+
+def click(click_time = 0.1, pos: (int, int)):
+    win32api.SetCursorPos((list(pos)[0], list(pos)[1]))
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0)
+    time.sleep(click_time)
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0)
+    return 0
