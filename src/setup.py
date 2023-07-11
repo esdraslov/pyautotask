93373abf3899da9c2daa312e5058874c5c3b5223
esdraslov <103470150+esdraslov@users.noreply.github.com>
2023-07-11 00:56:44 +0000

    setup fixed

--- src/setup.py
+++ src/setup.py
@@ -4,6 +4,6 @@ from setuptools import setup, find_packages
 setup(
     name="pyautotask",
     version="0.0.1",
-    packages=find_packages('./epacks/'),
+    packages=find_packages('./epack/'),
     install_requires=["pywin32", "pyautogui", "pynput"]
 )
