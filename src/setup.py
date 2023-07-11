c27d246b90d786611199939a7fd1c9691d814c44
esdraslov <103470150+esdraslov@users.noreply.github.com>
2023-07-11 00:48:43 +0000

    added setup.py

---
+++ src/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="pyautotask",
+    version="0.0.1",
+    packages=find_packages('./epacks/'),
+    install_requires=["pywin32", "pyautogui", "pynput"]
+)
